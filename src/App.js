5a9cae63290c96ac273a558d9bb97252f7867a7c
Nikhil <nikhilsharmamusic2000@gmail.com>
2021-06-29 17:53:16 +0530

    Added SignUp Page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Route, Switch } from 'react-router';
 import './App.css';
 
 import Layout from './components/Layout/Layout'
-import { Home, Login } from './pages/PageSrc'
+import { Home, Login, Signup } from './pages/PageSrc'
 
 function App() {
   return (
     <Layout>
       <Switch>
+        <Route path="/signup" exact component={Signup} />
         <Route path="/login" exact component={Login} />
         <Route path="/" exact component={Home} />
       </Switch>
