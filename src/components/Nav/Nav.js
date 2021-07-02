9fce7b4476cce69e30bbef5278fe49b4ee1c4ec6
jayesh purohit <jayeshpurohit456789@gmail.com>
2021-07-02 14:41:07 -0700

    Navbar changes added

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import './Nav.css'
+import React, {useState} from "react";
+import "./Nav.css";
+import NavLink from "./../UI/NavLink/NavLinks";
 
-import NavLink from './../UI/NavLink/NavLinks'
-
-export default function Nav(props){
-    return (
-        <nav className="header__nav">
-            <ul className="header__ul">
-                <NavLink href="/about" >About</NavLink>
-                <NavLink href="/feature" >Courses</NavLink>
-                <NavLink href="/feature" >Quiz</NavLink>
-                <NavLink href="/feature" >Guidance</NavLink>
-            </ul>
-        </nav>
-    )
+export default function Nav(props) {
+    
+  return (
+    <nav className="header__nav ">
+      <ul className="header__ul">
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/feature">Courses</NavLink>
+        <NavLink href="/feature">Quiz</NavLink>
+        <NavLink href="/about">About</NavLink>
+      </ul>
+    </nav>
+  );
 }
