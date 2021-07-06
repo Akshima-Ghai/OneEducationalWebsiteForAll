28143a0c2b71b113d2e68a081f9782ff1f9e9b9c
urmi_256 <urmi.chauhan05@gmail.com>
2021-07-06 18:31:29 +0530

    Frontpg

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import { Route, Switch } from "react-router";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Layout from "./components/Layout/Layout";
-import { Home, Login, Signup, About, FAQ, Section} from "./pages/PageSrc";
+import { Home, Login, Signup, About, FAQ, Section,Frontpg} from "./pages/PageSrc";
 
 function App() {
   return (
     <Switch>
+      <Route path="/" exact component={Frontpg} />
       <Layout>
         <Route path="/section" exact component={Section} />
         <Route path="/about" exact component={About} />
         <Route path="/signup" exact component={Signup} />
         <Route path="/login" exact component={Login} />
         <Route path="/faq" exact component={FAQ} />
-        <Route path="/" exact component={Home} />
+        <Route path="/home" exact component={Home} />
        
         <Footer />
       </Layout>
---
+++ src/pages/Frontpg/Frontpg.js
@@ -0,0 +1,54 @@
+import React from "react";
+import ImgSrc from "../../shared/ImgSrc";
+import "./Frontpg.css";
+import Admin from "../../components/UI/admincard/Admin";
+
+
+/* -------------------GO TO HOME PAGE BY (/home) THIS IS THE FRONT PAGE ------------------------ */
+const Frontpg = () => {
+  return (
+    <section className="user">
+      <div className="wrapper context">
+        <div className="wrapper-container">
+          <div className="txt">
+            <div className="static-txt">
+              <span>Welcome to</span>
+            </div>
+            <div className="dynamic-txts">
+              <span>LearnZania</span>
+            </div>
+          </div>
+          <img
+            src="https://www.metrilo.com/images/illustrations/email.svg"
+            alt=""
+          />
+        </div>
+        <div className="id-user">
+          <div className="userspan">
+            <span>Are you a ?</span>
+          </div>
+          <div className="user-adminchoose">
+            <Admin name="Student" Image={ImgSrc.Student} />
+            <Admin name="Teacher" Image={ImgSrc.Teacher} />
+          </div>
+        </div>
+      </div>
+
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+export default Frontpg;
