3f737c78dfeca6235357f9d5dfc64705dbfba3b2
urmi_256 <urmi.chauhan05@gmail.com>
2021-07-09 12:29:49 +0530

    Feature student page design

--- src/pages/Feature/Feature_student.js
+++ src/pages/Feature/Feature_student.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./Feature.css";
 import ImgSrc from "../../shared/ImgSrc";
-import { Fragment } from "react";
 const Feature_student = (props) => {
   return (
     <>
-      {/* This is for first bloc */}
+      {/* This is for first block */}
 
       <div className="container1">
         <div className="inside_container1">
@@ -24,10 +23,10 @@ const Feature_student = (props) => {
       {/* This is for second block */}
 
       <div className="container2">
+      <div className="inside_container2">
         <img className="image2" src={ImgSrc.FeatureStudentImage2} />
-        <div className="inside_container2">
           <div className="main_info2">
-            <h1 className="heading_block2_1">
+            <h1 className="heading_block2">
               About <label>Education</label>
             </h1>
             <div className="paragraph2">
@@ -37,49 +36,53 @@ const Feature_student = (props) => {
             </div>
           </div>
         </div>
-      </div>
-
-      {/* This is third block */}
-
-      <div className="container3">
-        <div className="inside_container3">
-          <div className="main_info3">
-            <h1 className="heading_block3_1">
+    
+        <div className="inside_container2 down">
+          <div className="main_info2">
+            <h1 className="heading_block2">
               Why <label>Education?</label>
             </h1>
-            <div className="paragraph3">
+            <div className="paragraph2">
               We provide , students with free and right guidance and help them
               in selecting their correct career path
             </div>
           </div>
-          <img className="image3" src={ImgSrc.FeatureStudentImage3} />
+          <img className="image2" src={ImgSrc.FeatureStudentImage3} />
         </div>
       </div>
 
-      {/* This is Fourth block */}
-
-      <div className="heading4">
+        {/* This is third block */} 
+      <div className="container3">
+      <div className="heading3">
         <h1>
           Choose Your <label>Role</label>
         </h1>
       </div>
-
-      <div className="container4">
-        <div className="inside_container4">
-          <img className="image4" src={ImgSrc.FeatureStudentImage4} />
-          <div className="main_info4">
-            <h1 className="heading_block4_1">
+      <div className="inside_cont3-box">
+        <div className="inside_container3">
+          <img className="image3" src={ImgSrc.FeatureStudentImage4} />
+          <div className="main_info3">
+            <h1 className="heading_block3">
               As a <label>Student</label>
             </h1>
-            <div className="paragraph4">
+            <div className="paragraph3">
               Start learning from the best teachers!
             </div>
-            <button className="btn4">Join</button>
+            <button className="btn3">Join</button>
           </div>
-          <div className="container5">
-          <img className="image5" src={ImgSrc.FeatureStudentImage5} />
           </div>
-          
+          <div className="inside_container3">
+          <img className="image3" src={ImgSrc.FeatureStudentImage5} />
+          <div className="main_info3">
+            <h1 className="heading_block3">
+              As a <label>Teacher</label>
+            </h1>
+            <div className="paragraph3">Are you ready to share your knowledge and experience ?
+            </div>
+            <button className="btn3">Join</button>
+          </div>
+          </div>
+         
         </div>
           
           
